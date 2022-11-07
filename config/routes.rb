56f81734d7fe57eb3524b7819a46adaf4da0f73f
Kirill <famavolatt@gmail.com>
2022-11-07 20:19:29 +0200

    Sing Up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get "about-us", to:"about#index", as: :about
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root to: "main#index"
+  get "about", to:"about#index", as: :about
+
+  get "sign_up", to:  "registrations#new"
+  post "sign_up", to: "registrations#create"
+ 
+  root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
