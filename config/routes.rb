498649080811b04b6d004dab2ebcbc7567212d0d
Kirill <famavolatt@gmail.com>
2022-11-09 12:22:52 +0200

    Sign In + Logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
   get "sign_up", to:  "registrations#new"
   post "sign_up", to: "registrations#create"
- 
+  
+  get "sign_in", to:  "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
